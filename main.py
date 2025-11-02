a5698f09a21aa894f9f6b326cdb3010c0d1efcc6
anandhu05 <anandhu@ombrulla.com>
2025-11-02 12:13:24 +0530

    feat(api): upload api completed

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import os
 import zipfile
 import csv
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, flash
+from datetime import datetime
+import pandas as pd
+
+# defining paths and folders
+UPLOAD_FOLDER = os.path.join('storage', 'raw_image')
+DATA_FOLDER = os.path.join('storage', 'meta_data')
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(DATA_FOLDER, exist_ok=True)
+CSV_PATH = os.path.join(DATA_FOLDER, 'upload_details.csv')
+
 import re
 
 pattern = re.compile('[\W_]+')
@@ -11,31 +21,23 @@ def normalize(name):
 app = Flask(__name__)
 #Auto-reload templates without restarting the server
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-
+app.secret_key = "erwt67q8w8s"
 
 @app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
 
 
-# folders
-UPLOAD_FOLDER = 'storage'
-DATA_FOLDER = 'data'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-os.makedirs(DATA_FOLDER, exist_ok=True)
-
-CSV_PATH = os.path.join(DATA_FOLDER, 'images.csv')
-
-
 @app.route('/upload', methods=['POST'])
 def upload():
+    msg = ""
 
-    #checking all arguments existing
+    # checking all arguments existing
     if 'zipfile' not in request.files:
-        msg= "No image file"
+        msg = "No image file"
 
     if 'csv' not in request.files:
-        msg= "No csv file"
+        msg = "No csv file"
 
     img_file = request.files['zipfile']
     csv_file = request.files['csv']
@@ -48,10 +50,11 @@ def upload():
     if not csv_file.filename.lower().endswith('.csv'):
         msg = "Please upload a .csv file"
 
-    folder = normalize(img_file.filename.split(".")[0])
-
-
+    if msg:
+        flash(msg, 'error')
+        return redirect(url_for('home'))
     if img_file.filename.lower().endswith('.zip'):
+        # A temporary zipfile
         zip_path = os.path.join(UPLOAD_FOLDER, 'uploaded.zip')
         img_file.save(zip_path)
 
@@ -60,33 +63,73 @@ def upload():
                 # Skip directories
                 if member.is_dir():
                     continue
-                
+
                 # Get only the filename (remove folder paths inside ZIP)
                 filename = os.path.basename(member.filename)
                 if not filename:
-                    continue  # skip empty names
-                
+                    # skip empty names
+                    continue
+
                 # Target file path (flattened)
                 target_path = os.path.join(UPLOAD_FOLDER, filename)
-                
+
                 # Extract the file
                 with zip_ref.open(member) as source, open(target_path, "wb") as target:
                     target.write(source.read())
         # Collect image details
         image_data = []
+        current_datetime = datetime.now()
+        # formatting date and time
+        date = current_datetime.strftime("%Y-%m-%d")
+        time = current_datetime.strftime("%Y-%m-%d")
+
         for filename in os.listdir((UPLOAD_FOLDER)):
-            print("--- filename ---", filename)
-            if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+            if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
                 path = os.path.join(UPLOAD_FOLDER, filename)
                 size_kb = round(os.path.getsize(path) / 1024, 2)
-                print("---- info ---", [filename, size_kb, ""])
-                image_data.append([filename, size_kb, ""])  # last column for user notes
+                image_data.append([
+                                    filename,
+                                    size_kb,
+                                    date,
+                                    time,
+                                    csv_file.filename.lower(),
+                                    "Not Processed"])
 
         # Write to CSV
         with open(CSV_PATH, 'w', newline='', encoding='utf-8') as f:
             writer = csv.writer(f)
-            writer.writerow(['Name', 'Size (KB)', 'Notes'])
+            writer.writerow(['name', 'size(KB)', 'date', 'time', 'meta_file', 'status'])
             writer.writerows(image_data)
+
+        # removing tempfile
+        os.remove(zip_path)
+
+    if csv_file.filename.lower().endswith('.csv'):
+
+        filepath = os.path.join(DATA_FOLDER,  csv_file.filename.lower())
+        csv_file.save(filepath)
+
+        try:
+            # Read CSV
+            df = pd.read_csv(filepath)
+
+            # Normalize column names
+            df.columns = df.columns.map(normalize)
+
+            # Check for required columns
+            required_columns = {"filename",	"brightness", "contrast", "sharpness"}
+
+            missing = required_columns - set(df.columns)
+            if missing:
+                flash(f"Missing required columns {list(missing)}", 'error')
+                return redirect(url_for('home'))
+
+            #Save normalized CSV
+            df.to_csv(filepath, index=False)
+
+        except Exception as e:
+            flash(f"error {str(e)}", 'error')
+            return redirect(url_for('home'))
 
     return redirect(url_for('show_images'))
 
